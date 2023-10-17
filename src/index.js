84cb9c51ee9c9c99ec4b232e346b5697fbdf4a51
sherget15 <herget06@gmail.com>
2023-10-17 15:42:31 -0400

    trying to deploy, restructuring files and folders

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import main from './main';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Router>
+      <App />  
+    </Router>
+  </React.StrictMode>
+);
